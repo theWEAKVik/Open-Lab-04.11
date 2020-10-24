efb137f3d6f7a0c3610fabb34a020c9ab5da0d27
Viktor <krpectv57@gmail.com>
2020-10-24 16:15:44 +0200

    /

--- Open-Lab-04.11/StringTools.cs
+++ Open-Lab-04.11/StringTools.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Open_Lab_04._11
 {
@@ -6,7 +8,10 @@ namespace Open_Lab_04._11
     {
         public string AlphabetSoup(string str)
         {
-            throw new NotImplementedException();
+            char[] pismena = str.ToCharArray();
+            Array.Sort(pismena);
+            string hotovo = string.Join("", pismena);
+            return hotovo;         
         }
     }
 }
